cb37123a801ac548053c3526e848680d0a9e7b42
zer2 <zer2@cornell.edu>
2023-12-28 15:48:19 -0500

    Update process_player_data.py

--- process_player_data.py
+++ process_player_data.py
@@ -35,7 +35,7 @@ def calculate_scores_from_coefficients(player_stats
 
 def process_player_data(player_stats, coefficients, psi):
 
-  player_stats[counting_statistics + volume_statistics] = player_stats[counting_statistics + volume_statistics] * ( 1- player_stats['No Play %'] * psi) 
+  player_stats[counting_statistics + volume_statistics] = player_stats[counting_statistics + volume_statistics].mul(( 1- player_stats['No Play %'] * psi), axis = 0)
   player_stats[percentage_statistics] = player_stats[percentage_statistics]/100 #adjust from the display
 
   g_scores = calculate_scores_from_coefficients(player_stats, coefficients, 1,1)
