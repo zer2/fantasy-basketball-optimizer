bb32c23e7348ddf947a0a7efea37814f3e990eb4
zer2 <zer2@cornell.edu>
2023-12-27 20:40:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
+from code import code
 
 st.markdown(f"Weekly player projections below: feel free to edit")
 
 df = pd.read_csv('./predictions.csv').set_index('player')
 
 edited_df = st.data_editor(df) # 👈 An editable dataframe
+
+st.markdown(code())
+
