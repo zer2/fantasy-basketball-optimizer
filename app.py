a178c93b4c558d6d7d604bded99ba020994678dc
zer2 <zer2@cornell.edu>
2023-12-27 19:14:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import streamlit as st
-import pandas as pd
-
 df = pd.DataFrame(
     [
         {"command": "st.selectbox", "rating": 4, "is_widget": True},
@@ -9,4 +6,8 @@ df = pd.DataFrame(
     ]
 )
 
-st.dataframe(df, use_container_width=True)
+df = load_data()
+edited_df = st.data_editor(df) # 👈 An editable dataframe
+
+favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
