59633f3996e6f1bdc98012ee70f7ac44ea953e35
zer2 <zer2@cornell.edu>
2023-12-28 13:36:09 -0500

    Update process_player_data.py

--- process_player_data.py
+++ process_player_data.py
@@ -1,4 +1,6 @@
 #1: based on player stats: calculate coefficients and score tables and such
 
-def process_player_data():
+def process_player_data(edited_df):
+  counting_statistics = ['Points','Rebounds','Steals']
+  
   return 'Process player data'
