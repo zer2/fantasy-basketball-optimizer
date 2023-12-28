502c9265268290ca1f5335eeee5dd03cc69b2e94
zer2 <zer2@cornell.edu>
2023-12-28 09:31:57 -0500

    Update app.py
    Rounded the predictions, for easier viewing

--- app.py
+++ app.py
@@ -10,10 +10,8 @@ color_map = {'C' : 'yellow'
              ,'SF' : 'green'
              ,'SG' : 'red'
              ,'PG' : 'red'}
-             
 
-
-df = pd.read_csv('./predictions.csv').set_index('player')
+df = pd.read_csv('./predictions.csv').set_index('player').round(2)
 
 edited_df = st.data_editor(df) # 👈 An editable dataframe
 
