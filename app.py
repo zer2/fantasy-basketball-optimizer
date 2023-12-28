388106643e0cbdbb963733c7cdda30e4b55380a0
zer2 <zer2@cornell.edu>
2023-12-27 20:31:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,3 @@ import pandas as pd
 df = pd.read_csv('./predictions.csv')
 
 edited_df = st.data_editor(df) # 👈 An editable dataframe
-
-favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
-st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
