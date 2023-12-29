91fe2ba15657465eb10a83f46625df9d5ebd0ec9
zer2 <zer2@cornell.edu>
2023-12-29 13:06:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ with tab3:
                , winner_take_all = winner_take_all)
 
     players_chosen = listify(selections_editable)
-    my_players = selections_editable['Drafter ' + str(seat)].dropna()
+    my_players = [p for p in selections_editable['Drafter ' + str(seat)] if len(p) > 0]
 
     res = H.get_h_scores(player_stats, my_players, players_chosen)
     st.dataframe(res)
