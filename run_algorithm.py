fba03c995bc3867803115772e927dc8f4c62bd34
zer2 <zer2@cornell.edu>
2023-12-29 12:07:39 -0500

    Update run_algorithm.py

--- run_algorithm.py
+++ run_algorithm.py
@@ -1,9 +1,6 @@
 #2: based on drafting situation: run gradient descent, get results
 
-def run_algorithm():
-  return 'Run algorithm'
-
-#below here experimental
+import numpy as np
 
 class HAgent():
 
