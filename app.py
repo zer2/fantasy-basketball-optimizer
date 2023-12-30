e68383409a177e8734b33b59477f1f3b2a917a35
zer2 <zer2@cornell.edu>
2023-12-29 22:57:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -192,9 +192,8 @@ with tab3:
   
       generator = H.get_h_scores(player_stats, my_players, players_chosen)
 
-      print('Hello')
       for i in range(n_iterations):
-        print(str(i))
+        st.write(str(i))
         c, res = next(generator) 
         res = res.sort_values(ascending = False)
         st.dataframe(res)
