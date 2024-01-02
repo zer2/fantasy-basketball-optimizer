0b95c00dce918135c106855ffccc3fcaf46213a1
zer2 <zer2@cornell.edu>
2024-01-02 09:41:11 -0500

    actually fixed the duplication this time

--- app.py
+++ app.py
@@ -191,10 +191,12 @@ with tab3:
       z_tab, g_tab = st.tabs(["Z-score", "G-score"])
       with z_tab:
         team_stats = z_scores[z_scores.index.isin(team_selections)]
+        team_stats.loc['Total', :] = team_stats.sum(axis = 0)
         team_stats = team_stats.round(2)
         z_display = st.dataframe(team_stats)
       with g_tab:
         team_stats = g_scores[g_scores.index.isin(team_selections)]
+        team_stats.loc['Total', :] = team_stats.sum(axis = 0)
         team_stats = team_stats.round(2)
         g_display = st.dataframe(team_stats)
 
@@ -241,8 +243,6 @@ with tab3:
             z_scores_unselected = st.dataframe(z_scores[~z_scores.index.isin(listify(selections_editable))])
             
           with subtab2:
-            g_scores.loc[:,'Total'] = g_scores.sum(axis = 1)
-            g_scores.sort_values('Total', ascending = False, inplace = True)
             g_scores = g_scores.round(2)
             g_scores_unselected = st.dataframe(g_scores[~g_scores.index.isin(listify(selections_editable))])
         
