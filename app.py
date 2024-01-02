feed6b347a3f6891721757a575ce60953623a685
zer2 <zer2@cornell.edu>
2024-01-02 08:44:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -246,7 +246,7 @@ with tab3:
                 st.plotly_chart(make_progress_chart(all_res))
 
             with weight_tab:
-              st.dataframe(c[res.index])
+              st.dataframe(c.loc[res.index])
             
 
  
