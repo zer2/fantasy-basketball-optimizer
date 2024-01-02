5011bfbd66b643e47faf8fad0bc3e0bb12ce6e06
zer2 <zer2@cornell.edu>
2024-01-02 13:20:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -201,7 +201,7 @@ with tab3:
         team_stats.loc['Total', :] = team_stats.sum(axis = 0)
         team_stats.loc['Expected', :] = expected
 
-        team_stats - team_stats.style.format("{:.2%}").applymap(styler)
+        team_stats = team_stats.style.format("{:.2%}").applymap(styler)
 
         z_display = st.dataframe(team_stats.style)        
         
