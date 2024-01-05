b242de9c46cab41d344dc1289002f82b75a698fe
zer2 <zer2@cornell.edu>
2024-01-04 22:24:38 -0500

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -97,7 +97,7 @@ def make_progress_chart(res):
 
     fig.update_layout(yaxis={'visible': False, 'showticklabels': True})
 
-    fig,update_layout(margin=go.layout.Margin(
+    fig.update_layout(margin=go.layout.Margin(
                                     l=0, #left margin
                                     r=0, #right margin
                                     b=0, #bottom margin
