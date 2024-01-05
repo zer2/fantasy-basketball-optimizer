85b510fa144768dcdda4c06f036770b712a75d6e
zer2 <zer2@cornell.edu>
2024-01-04 22:09:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -232,7 +232,7 @@ with tab3:
         team_stats.loc['Total', :] = team_stats.sum(axis = 0)
         team_stats.loc['Expected', :] = expected
 
-        team_stats = team_stats.style.format("{:.2}").applymap(other_styler).applymap(stat_styler, subset = pd.IndexSlice[team_players, counting_statistics + percentage_statistics])
+        team_stats = team_stats.style.format("{:.2}").map(other_styler).map(stat_styler, subset = pd.IndexSlice[team_players, counting_statistics + percentage_statistics])
 
         z_display = st.dataframe(team_stats)        
         
@@ -244,7 +244,7 @@ with tab3:
         team_stats.loc['Total', :] = team_stats.sum(axis = 0)
         team_stats.loc['Expected', :] = expected
 
-        team_stats = team_stats.style.format("{:.2}").applymap(other_styler).applymap(stat_styler, subset = pd.IndexSlice[team_players, counting_statistics + percentage_statistics])
+        team_stats = team_stats.style.format("{:.2}").map(other_styler).map(stat_styler, subset = pd.IndexSlice[team_players, counting_statistics + percentage_statistics])
 
 
         g_display = st.dataframe(team_stats)
@@ -255,12 +255,12 @@ with tab3:
     
       with subtab1:
         z_scores_unselected = z_scores[~z_scores.index.isin(listify(selections_editable))]
-        z_scores_unselected = z_scores_unselected.style.format("{:.2}").applymap(other_styler).applymap(stat_styler, subset = pd.IndexSlice[:,counting_statistics + percentage_statistics])
+        z_scores_unselected = z_scores_unselected.style.format("{:.2}").map(other_styler).map(stat_styler, subset = pd.IndexSlice[:,counting_statistics + percentage_statistics])
         z_scores_display = st.dataframe(z_scores_unselected)
         
       with subtab2:
         g_scores_unselected = g_scores[~g_scores.index.isin(listify(selections_editable))]
-        g_scores_unselected = g_scores_unselected.style.format("{:.2}").applymap(other_styler).applymap(stat_styler, subset = pd.IndexSlice[:,counting_statistics + percentage_statistics])
+        g_scores_unselected = g_scores_unselected.style.format("{:.2}").map(other_styler).map(stat_styler, subset = pd.IndexSlice[:,counting_statistics + percentage_statistics])
         g_scores_display = st.dataframe(g_scores_unselected)
     
       with subtab3:
