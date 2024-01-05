75558687cdaaae3f55e74117c0934b74e2fab0ff
zer2 <zer2@cornell.edu>
2024-01-05 09:35:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -234,7 +234,7 @@ with tab3:
 
         team_stats = team_stats.style.format("{:.2}").map(other_styler).map(stat_styler, subset = pd.IndexSlice[team_players, counting_statistics + percentage_statistics])
 
-        z_display = st.dataframe(team_stats)        
+        z_display = st.dataframe(team_stats, use_container_width = True)        
         
       with g_tab:
         team_stats = g_scores[g_scores.index.isin(team_selections)]
@@ -247,7 +247,7 @@ with tab3:
         team_stats = team_stats.style.format("{:.2}").map(other_styler).map(stat_styler, subset = pd.IndexSlice[team_players, counting_statistics + percentage_statistics])
 
 
-        g_display = st.dataframe(team_stats)
+        g_display = st.dataframe(team_stats, use_container_width = True)
         
     with cand_tab:
 
