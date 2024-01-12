e04875bca0e830b92b487359528266cb2160066c
zer2 <zer2@cornell.edu>
2024-01-11 23:18:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -363,7 +363,7 @@ with tab4:
 
             _, res= next(H.get_h_scores(player_stats, mod_my_players, players_chosen))
 
-            res = res - base_score
+            res = res - base_score.values[0]
             res = res.sort_values(ascending = False).round(3)
             res.name = 'H-score differential'
 
