4939268ee0e786a97ebef0e059732ea3481e8d6a
zer2 <zer2@cornell.edu>
2024-01-13 22:21:39 -0500

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,13 +1,23 @@
 import pandas as pd
 from nba_api.stats.endpoints import leaguegamefinder, playergamelogs
+from streamlit import cache_resource
 
+@cache_resource(ttl = '1d') 
 def get_data(season):
   season_str = str(season -1) + '-' + str(season -2000)
   pgl_df = pd.concat(
       [
           playergamelogs.PlayerGameLogs(
-              season_nullable="2023-24", season_type_nullable=season_type
+              season_nullable=season_str, season_type_nullable=season_type
           ).player_game_logs.get_data_frame()
           for season_type in ["Regular Season"]
       ]
   )
+  
+  relevant_columns = ['PLAYER_NAME','PTS','REB','AST','STL','BLK','FG3M','TOV','FTA','FTM','FGA','FGM','GAME_DATE']
+  pgl_df = pgl_df[relevant_columns]
+
+def process_game_level_data(df):
+  agg_df = df.groupy('PLAYER_NAME').mean()
+  agg_df.loc[:,'Free Throw %'] = df['FTM']/df['FTA']
+  agg_df.loc[:,'Field Goal %'] = df['FGA']/df['FGM']
