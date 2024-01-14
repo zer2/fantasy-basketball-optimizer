4ca55b48458a538b422465610620d12ccb94c9a6
zer2 <zer2@cornell.edu>
2024-01-14 14:51:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from process_player_data import process_player_data
 from run_algorithm import HAgent, analyze_trade
 from helper_functions import listify, make_progress_chart, read_markdown_file, stat_styler, styler_a,styler_b, styler_c
 from get_data import get_historical_data, get_current_season_data
-from pathlib import Path
 import numpy as np
 import os 
 
