f668c267ee273eac5dfd437ba8e5b04d04676344
zer2 <zer2@cornell.edu>
2024-01-14 14:21:14 -0500

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -49,7 +49,7 @@ def get_current_season_data(season = 2024):
 
   data_dict = {str(season) + '-Four Week Average' : process_game_level_data(four_week_subset, player_metadata)
                ,str(season) + '-Two Week Average' : process_game_level_data(two_week_subset, player_metadata)
-               ,str(season) + '-Full Season' :  process_game_level_data(two_week_subset, player_metadata)
+               ,str(season) + '-Full Season' :  process_game_level_data(full_subset, player_metadata)
               }
   return data_dict 
 
