fcf3aa816e8ff7e08ba0be92c6fb40383398e38e
zer2 <zer2@cornell.edu>
2024-01-24 17:09:54 -0500

    Update get_data.py

--- src/get_data.py
+++ src/get_data.py
@@ -153,24 +153,24 @@ def get_darko_long_term(all_darko, expected_minutes, params):
     all_darko = all_darko.drop(columns = ['Minutes'])
     darko_long_term = all_darko.merge(expected_minutes, left_index = True, right_index = True)
   
-    possesions = darko_long_term['Minutes']/48 * darko_long_term['Pace']
+    possesions = darko_long_term['Minutes']/48 * darko_long_term['Pace']/100
   
-    free_throw_attempts = possesions * darko_long_term.loc[:,'FTA/100'] * 100  
+    free_throw_attempts = possesions * darko_long_term.loc[:,'FTA/100'] 
     free_throws_made = free_throw_attempts * darko_long_term.loc[:,'Free Throw %']
 
-    three_attempts = possesions * darko_long_term.loc[:,'FG3A/100'] * 100 
+    three_attempts = possesions * darko_long_term.loc[:,'FG3A/100'] *
     threes_made = darko_long_term['FG3%']
 
-    two_attempts = possesions * (darko_long_term.loc[:,'FGA/100'] - darko_long_term.loc[:,'FG3A/100'] ) * 100
+    two_attempts = possesions * (darko_long_term.loc[:,'FGA/100'] - darko_long_term.loc[:,'FG3A/100'] ) 
     twos_made = two_attempts * darko_long_term['FG2%']
  
     darko_long_term.loc[:,'Points'] = 3*threes_made + 2*twos_made + free_throws_made
-    darko_long_term.loc[:,'Rebounds'] = possesions * darko_long_term.loc[:,'REB/100'] * 100
-    darko_long_term.loc[:,'Assists'] = possesions * darko_long_term.loc[:,'AST/100'] * 100
-    darko_long_term.loc[:,'Steals'] = possesions * darko_long_term.loc[:,'STL/100'] * 100
-    darko_long_term.loc[:,'Blocks'] = possesions * darko_long_term.loc[:,'BLK/100'] * 100
+    darko_long_term.loc[:,'Rebounds'] = possesions * darko_long_term.loc[:,'REB/100'] 
+    darko_long_term.loc[:,'Assists'] = possesions * darko_long_term.loc[:,'AST/100'] 
+    darko_long_term.loc[:,'Steals'] = possesions * darko_long_term.loc[:,'STL/100'] 
+    darko_long_term.loc[:,'Blocks'] = possesions * darko_long_term.loc[:,'BLK/100'] 
     darko_long_term.loc[:,'Threes'] =  threes_made
-    darko_long_term.loc[:,'Turnovers'] = possesions * darko_long_term.loc[:,'TOV/100'] * 100
+    darko_long_term.loc[:,'Turnovers'] = possesions * darko_long_term.loc[:,'TOV/100'] 
     darko_long_term.loc[:,'Field Throw Attempts'] = free_throw_attempts
     darko_long_term.loc[:,'Field Goal Attempts'] = two_attempts + three_attempts
 
