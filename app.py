a911486a3bf8c6a6f1a2f8fe77958b9a44b0e2f0
zer2 <zer2@cornell.edu>
2024-01-28 11:03:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -501,9 +501,23 @@ with rank_tab:
                                            , subset = pd.IndexSlice[:,counting_statistics + percentage_statistics]
                                            , multiplier = z_score_player_multiplier)
     
-      z_scores_display = st.dataframe(z_scores_styled, hide_index = True)
+      z_scores_rank_display = st.dataframe(z_scores_styled, hide_index = True)
   with g_rank_tab:
-      st.markdown('Placeholder')
-  with h_rank_tab:
+      g_score_columns_original = g_scores.columns
+      g_scores.loc[:,'Rank'] = np.arange(g_scores.shape[0]) + 1
+      g_scores.loc[:,'Player'] = g_scores.index
+      g_scores = g_scores[['Rank','Player'] + counting_statistics + percentage_statistics + ['Total']]
+
+      g_scores_styled = g_scores.style.format("{:.2f}"
+                                             ,subset = pd.IndexSlice[:,counting_statistics + percentage_statistics + ['Total']]) \
+                                        .map(styler_a
+                                            , subset = pd.IndexSlice[:,['Total']]) \
+                                        .map(stat_styler
+                                           , subset = pd.IndexSlice[:,counting_statistics + percentage_statistics]
+                                           , multiplier = z_score_player_multiplier)
+    
+      g_scores_rank_display = st.dataframe(g_scores_styled, hide_index = True)  
+    
+    with h_rank_tab:
       st.markdown('Placeholder')
     
