68433e5038e1a6bb40ed90992359fd95c0877098
zer2 <zer2@cornell.edu>
2024-01-28 00:48:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -487,11 +487,13 @@ with rank_tab:
   z_rank_tab, g_rank_tab, h_rank_tab = st.tabs(['Z-score','G-score','H-score'])
 
   with z_rank_tab:
-      z_score_columns_original = z_scores.columns
-      z_scores.loc[:,'Rank'] = np.arange(len(z_scores))
-      z_scores = z_scores[['Rank'] + list(z_score_columns_original)]
-      z_scores_unselected_styled = z_scores.style.format("{:.2}").map(styler_a, subset = z_score_columns_original).map(stat_styler, subset = pd.IndexSlice[:,counting_statistics + percentage_statistics], multiplier = z_score_player_multiplier)
-      z_scores_display = st.dataframe(z_scores_unselected_styled)
+      z_scores_styled = z_scores.style.format("{:.2}").map(styler_a).map(stat_styler, subset = pd.IndexSlice[:,counting_statistics + percentage_statistics], multiplier = z_score_player_multiplier)
+
+      z_score_columns_original = z_scores_styled.columns
+      z_scores_styled.loc[:,'Rank'] = np.arange(len(z_scores_styled))
+      z_scores_styled = z_scores_styled[['Rank'] + list(z_score_columns_original)]
+    
+      z_scores_display = st.dataframe(z_scores_styled)
   with g_rank_tab:
       st.markdown('Placeholder')
   with h_rank_tab:
