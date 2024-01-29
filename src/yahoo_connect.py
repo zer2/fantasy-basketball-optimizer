854d6dfd4c6f2ac91e9782827d12cdfd69f4f2c8
zer2 <zer2@cornell.edu>
2024-01-29 15:27:59 -0500

    Update yahoo_connect.py

--- src/yahoo_connect.py
+++ src/yahoo_connect.py
@@ -1,17 +1,26 @@
 from yahoo_oauth import OAuth2
 import yahoo_fantasy_api as yfa
+import streamlit as st
 
-oauth = OAuth2('consumer_key', 'consumer_secret')
+def get_yahoo_info(league_id = None):
+  yahoo_client_id = st.secrets["YAHOO_CLIENT_ID "]
+  yahoo_client_secret = st.secrets["d5d8701990a58bace6b47e58c3e173a43991949e"]
+  oauth = OAuth2(yahoo_client_id, yahoo_client_secret)
 
-gm = yfa.Game(oauth, 'nba')
-league_id = gm.league_ids(year=2023)[0]
-league = gm.to_league(league_id)
-
-rosters = { t['name'] : league.to_team(team_id).roster(21) for team_id, t in league.teams().items()}
-
-roster_dict = {team: [p['name'] for p in roster if p['selected_position'] != 'IL' ] for team, roster in rosters.items()}
-roster_df = pd.DataFrame.from_dict(roster_dict, orient='index')
-roster_df = roster_df.transpose()
-
-il_dict = {team: [p['name'] for p in roster if p['selected_position'] == 'IL' ] for team, roster in rosters.items()}
-all_il_players = [x for v in il_dict.values() for x in v]
+  #convert league ID to the right format, or grab the first team id if none is listed
+  if league_idea is None:
+    gm = yfa.Game(oauth, 'nba')
+    league_id = gm.league_ids()[0]
+  else:
+    league_id = '418.1.' + league_id 
+    
+  league = gm.to_league(league_id)
+  
+  rosters = { t['name'] : league.to_team(team_id).roster(21) for team_id, t in league.teams().items()}
+  
+  roster_dict = {team: [p['name'] for p in roster if p['selected_position'] != 'IL' ] for team, roster in rosters.items()}
+  roster_df = pd.DataFrame.from_dict(roster_dict, orient='index')
+  roster_df = roster_df.transpose()
+  
+  il_dict = {team: [p['name'] for p in roster if p['selected_position'] == 'IL' ] for team, roster in rosters.items()}
+  all_il_players = [x for v in il_dict.values() for x in v]
