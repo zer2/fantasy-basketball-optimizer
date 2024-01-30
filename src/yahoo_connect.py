b65e6705f6e628ea1379398688dda745cf1295ac
zer2 <zer2@cornell.edu>
2024-01-29 19:11:05 -0500

    Update yahoo_connect.py

--- src/yahoo_connect.py
+++ src/yahoo_connect.py
@@ -14,36 +14,6 @@ def nav_to(url):
 def get_yahoo_info(league_id):
     yahoo_client_id = st.secrets["YAHOO_CLIENT_ID"]
     yahoo_client_secret = st.secrets["YAHOO_CLIENT_SECRET"]
-
-    # Replace these values with your Yahoo API credentials
-    client_id = st.secrets["YAHOO_CLIENT_ID"]
-    client_secret = st.secrets["YAHOO_CLIENT_SECRET"]
-    redirect_uri = 'https://fantasy-basketball-optimizer-yleby8jedyrzhfwoycwuc8.streamlit.app/oath/callback'
-    
-    authorization_base_url = 'https://api.login.yahoo.com/oauth2/request_auth'
-    token_url = 'https://api.login.yahoo.com/oauth2/get_token'
-
-    # Step 1: Obtain authorization URL
-    yahoo = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=['openid', 'profile', 'email'])
-    authorization_url, state = yahoo.authorization_url(authorization_base_url)
-
-    print(f'Please go to {authorization_url} and authorize the application.')
-
-    # Step 2: Get the authorization response
-    redirect_response = input('Paste the full redirect URL here: ')
-
-    # Step 3: Get the access token
-    yahoo.fetch_token(token_url, authorization_response=redirect_response, client_secret=client_secret)
-
-    # Now you can use yahoo's session for authenticated requests
-
-    # Example: Get user's profile information
-    profile_url = 'https://api.login.yahoo.com/openid/v1/userinfo'
-    response = yahoo.get(profile_url)
-    user_data = response.json()
-
-    print('User Profile Information:')
-    print(user_data)
     
     redirect = 'https://api.login.yahoo.com/oauth2/request_auth?redirect_uri=oob&response_type=code&client_id=' + yahoo_client_id
     st.markdown("check out this [link](" + redirect + ")", unsafe_allow_html = True)
