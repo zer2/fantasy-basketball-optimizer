796059d089a0426a48befb0a0ecea630621aeb12
zer2 <zer2@cornell.edu>
2024-02-02 22:11:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -383,11 +383,11 @@ with Profiler():
 
           @st.cache_data()
           def make_z_styled(z_scores):  
-            z_scores_styled = z_scores.style.format("{:.2f}").map(styler_a).map(stat_styler, subset = pd.IndexSlice[:,counting_statistics + percentage_statistics], multiplier = z_score_player_multiplier)
+            st.session_state.z_scores_styled = z_scores.style.format("{:.2f}").map(styler_a).map(stat_styler, subset = pd.IndexSlice[:,counting_statistics + percentage_statistics], multiplier = z_score_player_multiplier)
               
           with subtab1:
-            make_z_styled(z_scores)
             unselected = z_scores.index[z_scores.index.isin(listify(selections_editable))]
+            z_scores_styled = st.session_state.z_scores_styled
             z_scores_styled_unselected = z_scores_styled.hide(unselected)
             z_scores_display = st.dataframe(z_scores_styled_unselected)
             
