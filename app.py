f1e4bddbd64d992b501f73f987d211f97796b428
zer2 <zer2@cornell.edu>
2024-02-23 11:22:32 -0500

    fixed stupid error

--- app.py
+++ app.py
@@ -493,9 +493,9 @@ with draft_tab:
             all_res = all_res + [score]
             #normalize weights by what we expect from other drafters
             
-            c = pd.DataFrame(c, index = score.index, columns = categories)/info['v'].T
+            c = pd.DataFrame(c, index = score.index, columns = get_categories())/info['v'].T
             
-            cdf_estimates.columns = categories
+            cdf_estimates.columns = get_categories()
             
             with placeholder.container():
   
