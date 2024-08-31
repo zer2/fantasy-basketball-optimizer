ceb19d2d0990ceb79cc11d4d4c6565fa3183825b
Zach Rosenof <zer2@cornell.edu>
2024-08-31 16:16:48 -0400

    removed an extraneous write statement

--- src/yahoo_connect.py
+++ src/yahoo_connect.py
@@ -300,7 +300,6 @@ def get_draft_results(league_id: str,_auth_path: str, player_metadata):
     try:
         draft_results = sc.get_league_draft_results()
     except Exception as e:
-        st.write(e)
         return None
             
     max_round = max([item.round for item in draft_results])
