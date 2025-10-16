a30ab323a602479a89463eed93031714b3347fa2
zer2 <zer2@cornell.edu>
2025-10-15 21:10:09 -0400

    fixed ESPN bug

--- src/data_retrieval/get_data.py
+++ src/data_retrieval/get_data.py
@@ -408,9 +408,12 @@ def map_player_names(df, source_name):
    else:
       player_name_column = 'PLAYER_NAME'
 
-   mapper_table = get_data_from_snowflake('PLAYER_MAPPING_VIEW').dropna(subset = [source_name]) \
-                                                                  .set_index(source_name)[player_name_column]
-      
-   #does not get here
-   df['Player'] = df['Player'].map(mapper_table).fillna(df['Player'])
+   #Only change player name if it is necessary. E.g. if the data source and platform are both ESPN, no mapping is necessary
+   if player_name_column == source_name:
+
+    mapper_table = get_data_from_snowflake('PLAYER_MAPPING_VIEW').dropna(subset = [source_name]) \
+                                                                    .set_index(source_name)[player_name_column]
+        
+    df['Player'] = df['Player'].map(mapper_table).fillna(df['Player'])
+
    return df
