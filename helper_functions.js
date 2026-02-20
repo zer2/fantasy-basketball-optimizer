6992bd421b3caa29026e4fffc432176ad555dd30
zer2 <zer2@cornell.edu>
2026-02-19 22:53:38 -0500

    got the barest of bones on the table

--- helper_functions.js
+++ helper_functions.js
@@ -1,13 +1,17 @@
 export function ExpandView(i){
     //this should also toggle the button from down to up 
     let ev = document.querySelector(`#EV${i}.expandedview`);
+    let evpopup = document.querySelector(`#PP${i}.playerpopup`);
 
     console.log(ev.style.display)
-    if (ev.style.display == '') { 
-        ev.style.display = 'block'
+    if (ev.style.display == 'block') { 
+        ev.style.display = 'none'
+        evpopup.textContent = '▼'
     }
     else {
-        ev.style.display = ''
+
+        ev.style.display = 'block'
+        evpopup.textContent = '▲'
     }
     console.log(ev.style.display)
 
